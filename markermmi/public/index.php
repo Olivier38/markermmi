d0754d198796039d8c0e59f15672a1bc0d648b48
Olivier Traveaux <olivier.traveaux@gmail.com>
2014-11-18 17:01:55 +0100

    y a fichier index.php changé et le fichier index.ttwig

--- markermmi/public/index.php
+++ markermmi/public/index.php
@@ -1,2 +1,16 @@
-<?php 
-	echo phpinfo();
+<?php
+// Auto chargement des dépendences Composer
+require_once '../vendor/autoload.php';
+
+// Création d'un loader TWIG,
+// pour qu'il recherche les templates dans le répertoire application/views
+$loader = new Twig_Loader_Filesystem('../application/views');
+
+// Création d'une instance de Twig
+$twig = new Twig_Environment($loader);
+
+// Compilation et Affichage du template (index.twig)
+// Dans le fichier index.twig, le code {{ name }}
+// sera remplacé par sa valeur dans le tableau ("World")
+
+echo $twig->render('index.twig', array('name' => 'World'));
